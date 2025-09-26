25b1ff628fedd6eead634e070882462e1d2385f9
Diffinable <naumenk.alexandr@gmail.com>
2025-09-26 14:53:37 +0300

    Add models

--- core/models.py
+++ core/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    first_name = models.CharField()
+    last_name = models.CharField()
+    username = models.CharField()
+    password = models.CharField()
+
+class UserTask(models.Model):
+    user = models.ForeignKey("User")
+    task = models.ForeignKey("Task")
+
+class Task(models.Model):
+    name = models.CharField()
+    description = models.CharField(blank=True, null=True)
+    status = models.ForeignKey("Status")
+    type = models.Choices(["feature", "bugfix", "hotfix"])
+    planned_time = models.DecimalField()
+    slug = models.CharField()
+
+class Status(models.Model):
+    name = models.CharField()
+
+class BranchesTask(models.Model):
+    name = models.CharField()
+    url = models.CharField(blank=True, null=True)
+    task = models.ForeignKey("Task")
+
